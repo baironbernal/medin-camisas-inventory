54127f5bc9072baf4a57780afbaff01a6fa3e19b
Bairon Bernal <baironbernal263@gmail.com>
2026-04-08 16:26:31 -0500

    fix signup wholesaler

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,23 +24,35 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name'         => 'required|string|max:255',
-            'email'        => 'required|string|email|max:255|unique:users',
-            'first_name'   => 'nullable|string|max:255',
-            'phone_number' => 'required|string|max:255',
-            'last_name'    => 'nullable|string|max:255',
-            'password'     => 'required|string|min:8',
+            'name'             => 'required|string|max:255',
+            'email'            => 'required|string|email|max:255|unique:users',
+            'first_name'       => 'nullable|string|max:255',
+            'phone_number'     => 'required|string|max:255',
+            'last_name'        => 'nullable|string|max:255',
+            'password'         => 'required|string|min:8',
+            'whatsapp_number'  => 'nullable|string|max:255',
+            'city'             => 'nullable|string|max:255',
+            'selling_channel'  => 'nullable|string|max:255',
+            'clothing_type'    => 'nullable|string|max:255',
+            'selling_location' => 'nullable|string|max:255',
+            'business_name'    => 'nullable|string|max:255',
         ]);
 
         $nameParts = explode(' ', trim($request->name), 2);
 
         $user = User::create([
-            'name'         => $request->name,
-            'first_name'   => $request->first_name ?? ($nameParts[0] ?? $request->name),
-            'last_name'    => $request->last_name  ?? ($nameParts[1] ?? ''),
-            'phone_number' => $request->phone_number,
-            'email'        => $request->email,
-            'password'     => Hash::make($request->password),
+            'name'             => $request->name,
+            'first_name'       => $request->first_name ?? ($nameParts[0] ?? $request->name),
+            'last_name'        => $request->last_name  ?? ($nameParts[1] ?? ''),
+            'phone_number'     => $request->phone_number,
+            'email'            => $request->email,
+            'password'         => Hash::make($request->password),
+            'whatsapp_number'  => $request->whatsapp_number,
+            'city'             => $request->city,
+            'selling_channel'  => $request->selling_channel,
+            'clothing_type'    => $request->clothing_type,
+            'selling_location' => $request->selling_location,
+            'business_name'    => $request->business_name,
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
